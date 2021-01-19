f6f5d7b36b5a74271d99897f9c64c9ed0a1e070a
Antonio Pifferi <antonio.pifferi@polimi.it>
2021-01-19 15:10:17 +0100

    nuovo git

---
+++ TestGit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 13:00:38 2021
+
+@author: anton
+"""
+from numpy import *
+from matplotlib.pyplot import *
+
+x=arange(0,100)
+y=log(x)
+figure()
+plot(x,y)
+show()
